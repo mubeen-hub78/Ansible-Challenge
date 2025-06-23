c6b445c0370487c6feaad74d69039f564fa5bd09
mubeen-hub78 <mubeensyed7897@outlook.com>
2025-06-23 17:09:23 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,10 @@
-output "inventory" {
-  value = <<EOT
+inventory = <<EOT
 [frontend]
-c8.local ansible_host=${aws_instance.frontend.public_ip} ansible_user=ec2-user ansible_python_interpreter=/usr/bin/python3
+# Remove ansible_python_interpreter for c8.local so it defaults to Python 2 for initial raw commands
+c8.local ansible_host=${aws_instance.frontend.public_ip} ansible_user=ec2-user
 
 [backend]
+# Keep ansible_python_interpreter for u21.local as its Python 3.12 is sufficient
 u21.local ansible_host=${aws_instance.backend.public_ip} ansible_user=ubuntu ansible_python_interpreter=/usr/bin/python3
 
 [redhat_hosts]
@@ -11,5 +12,5 @@ c8.local
 
 [debian_hosts]
 u21.local
+
 EOT
-}

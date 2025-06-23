cf02684153b9c73b06fd5ba7ab8f4870ab1b33e5
mubeen-hub78 <mubeensyed7897@outlook.com>
2025-06-23 16:42:53 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,10 +1,9 @@
-output "inventory" {
-  value = <<EOT
+inventory = <<EOT
 [frontend]
-c8.local ansible_host=${aws_instance.frontend.public_ip} ansible_user=ec2-user ansible_python_interpreter=/usr/bin/python3.8
+c8.local ansible_host=${aws_instance.frontend.public_ip} ansible_user=ec2-user ansible_python_interpreter=/usr/bin/python3
 
 [backend]
-u21.local ansible_host=${aws_instance.backend.public_ip} ansible_user=ubuntu ansible_python_interpreter=/usr/bin/python3.8
+u21.local ansible_host=${aws_instance.backend.public_ip} ansible_user=ubuntu ansible_python_interpreter=/usr/bin/python3
 
 [redhat_hosts]
 c8.local
@@ -12,4 +11,3 @@ c8.local
 [debian_hosts]
 u21.local
 EOT
-}

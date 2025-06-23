f1d652d8d6546698786b3f71ca4731f921d78316
mubeen-hub78 <mubeensyed7897@outlook.com>
2025-06-23 14:59:19 +0530

    Update main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,37 @@
+resource "aws_instance" "frontend" {
+  ami           = "ami-0f3f13f145e66a0a3" # Amazon Linux 2 (64-bit x86)
+  instance_type = "t2.micro"
+  key_name      = var.key_name
+  subnet_id     = var.subnet_id
+  tags = {
+    Name = "c8.local"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              hostnamectl set-hostname c8.local
+              EOF
+}
+
+resource "aws_instance" "backend" {
+  ami           = "ami-020cba7c55df1f615" # Ubuntu Server 24.04 LTS (64-bit x86)
+  instance_type = "t2.micro"
+  key_name      = var.key_name
+  subnet_id     = var.subnet_id
+  tags = {
+    Name = "u21.local"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              hostnamectl set-hostname u21.local
+              EOF
+}
+
+output "frontend_ip" {
+  value = aws_instance.frontend.public_ip
+}
+
+output "backend_ip" {
+  value = aws_instance.backend.public_ip
+}

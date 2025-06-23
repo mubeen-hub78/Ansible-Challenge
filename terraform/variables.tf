9a54ad724ec44ae04a0dcbcc0d276845bccc676b
mubeen-hub78 <mubeensyed7897@outlook.com>
2025-06-23 17:42:22 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,13 @@
+# terraform/variables.tf
+
 variable "key_name" {
   description = "The name of the existing EC2 Key Pair to access the instances"
   type        = string
-  default     = "Virginia"
+  default     = "Virginia" # Your specified key name
 }
 
 variable "subnet_id" {
   description = "The Subnet ID where EC2 instances will be launched"
   type        = string
-  default     = "subnet-03edc94baf3c761fe"
+  default     = "subnet-03edc94baf3c761fe" # Your specified subnet ID
 }

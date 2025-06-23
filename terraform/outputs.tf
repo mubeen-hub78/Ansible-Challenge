b12b18929c13d1a7bfe87063fb2786f0208760a4
mubeen-hub78 <mubeensyed7897@outlook.com>
2025-06-23 16:16:35 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,5 +5,11 @@ c8.local ansible_host=${aws_instance.frontend.public_ip} ansible_user=ec2-user
 
 [backend]
 u21.local ansible_host=${aws_instance.backend.public_ip} ansible_user=ubuntu
+
+[redhat_hosts]
+c8.local
+
+[debian_hosts]
+u21.local
 EOT
 }
